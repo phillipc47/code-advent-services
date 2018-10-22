aea1265cd4dc60ff504bd59a0ed1fd5111ade095
Phillip Casey <Phillip.Casey@navihealth.com>
2018-10-22 08:42:13 -0500

    Adding JSON Formatting Options

--- API/ReverseCaptcha/Models/CalculationResult.cs
+++ API/ReverseCaptcha/Models/CalculationResult.cs
@@ -4,11 +4,8 @@ namespace API.ReverseCaptcha.Models
 {
 	public class CalculationResult
 	{
-		//TODO: Apply global json settings, lowercase, with - delimeters as well
-		[JsonProperty(PropertyName = "input")]
 		public string Input { get; set; }
 
-		[JsonProperty(PropertyName = "result")]
 		public string Result { get; set; }
 	}
 
