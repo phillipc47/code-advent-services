874d2befdf3728430f899844191e08de84a3d2c5
Phillip Casey <Phillip.Casey@navihealth.com>
2018-10-22 15:22:20 -0500

    End of the day, part way through tests

---
+++ API.Tests/ConfigurationData/Services/ConfigurationDataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using API.ConfigurationData.Models;
+using API.ConfigurationData.Repositories;
+using API.ConfigurationData.Services;
+using Moq;
+using Xunit;
+
+namespace API.Tests.ConfigurationData.Services
+{
+	public class ConfigurationDataServiceTest
+	{
+		[Fact]
+		public void Retrieves_Data_From_Repository()
+		{
+			var mockRepsository = new Mock<IConfigurationDataRepository>();
+
+			ConfigurationDataEntity expectedConfigurationData = new ConfigurationDataEntity();
+			mockRepsository.Setup(repository => repository.LoadConfigurationData()).Returns(expectedConfigurationData);
+
+			IConfigurationDataService service = new ConfigurationDataService(mockRepsository.Object);
+
+			var result = service.Lookup();
+
+			Assert.Same(expectedConfigurationData, result);
+			mockRepsository.Verify(x => x.LoadConfigurationData(), Times.Once);
+		}
+	}
+}
---
+++ API.Tests/ConfigurationDataControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using API.ConfigurationData;
+using API.ConfigurationData.Models;
+using API.ConfigurationData.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace API.Tests
+{
+	public class ConfigurationDataControllerTest
+	{
+		[Fact]
+		public void ReadConfiguration_Delegates_To_Service()
+		{
+			var mockRepository = new Mock<IConfigurationDataService>();
+			ConfigurationDataEntity expectedResult = new ConfigurationDataEntity();
+			mockRepository.Setup(service => service.Lookup()).Returns(expectedResult);
+
+			var controller = new ConfigurationDataController(mockRepository.Object);
+
+			var controllerResult = controller.ReadConfiguration();
+			Assert.NotNull(controllerResult);
+
+			var okResult = controllerResult.Result as OkObjectResult;
+			Assert.NotNull(okResult);
+			Assert.Equal(200, okResult.StatusCode);
+
+			//TODO: Check the result coming back okResult.Value
+		}
+	}
+}
