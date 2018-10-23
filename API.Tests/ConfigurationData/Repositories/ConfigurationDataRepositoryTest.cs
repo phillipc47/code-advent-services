3d58b997c9eb60840c042e30593adb464b3bfa2f
Phillip Casey <Phillip.Casey@navihealth.com>
2018-10-23 09:34:16 -0500

    Controller logic tested

---
+++ API.Tests/ConfigurationData/Repositories/ConfigurationDataRepositoryTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Tests.ConfigurationData.Repositories
+{
+	class ConfigurationDataRepositoryTest
+	{
+	}
+}
--- API/ConfigurationData/ConfigurationDataController.cs
+++ API/ConfigurationData/ConfigurationDataController.cs
@@ -1,6 +1,8 @@
-﻿using API.ConfigurationData.Models.Response;
+﻿using System.Collections.Generic;
+using API.ConfigurationData.Models;
 using API.ConfigurationData.Services;
 using Microsoft.AspNetCore.Mvc;
+using Response = API.ConfigurationData.Models.Response;
 
 namespace API.ConfigurationData
 {
@@ -10,6 +12,26 @@ namespace API.ConfigurationData
 	{
 		private IConfigurationDataService Service { get; }
 
+		private Response.ConfigurationDataResponse CreateResponse(ConfigurationDataEntity configurationDataEntity)
+		{
+			var configurationDataResponse = new Response.ConfigurationDataResponse()
+			{
+				Something = configurationDataEntity.Something,
+				ServiceEndpoints = new Dictionary<string, Response.ServiceEndpointDetail>()
+			};
+			return configurationDataResponse;
+		}
+
+		private void CopyServiceEndpoints(ConfigurationDataEntity configurationDataEntity, Response.ConfigurationDataResponse configurationDataResponse)
+		{
+			if (configurationDataEntity.ServiceEndpoints != null)
+			{
+				foreach (var entityEndpoint in configurationDataEntity.ServiceEndpoints)
+				{
+					configurationDataResponse.ServiceEndpoints.Add(entityEndpoint.Key, new Response.ServiceEndpointDetail() { Url = entityEndpoint.Value.Url });
+				}
+			}
+		}
 
 		public ConfigurationDataController(IConfigurationDataService service)
 		{
@@ -17,9 +39,15 @@ namespace API.ConfigurationData
 		}
 
 		[HttpGet]
-		public ActionResult<ConfigurationDataResponse> ReadConfiguration()
+		public ActionResult<Response.ConfigurationDataResponse> ReadConfiguration()
 		{
-			return Ok(Service.Lookup());
+			var configurationDataEntity = Service.Lookup();
+
+			//TODO: Automap
+			var configurationDataResponse = CreateResponse(configurationDataEntity);
+			CopyServiceEndpoints(configurationDataEntity, configurationDataResponse);
+
+			return Ok(configurationDataResponse);
 		}
 	}
 }
