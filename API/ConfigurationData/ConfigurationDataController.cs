2808a6ff4f4e0c915b3194d6f907a19ddef550e8
Phillip Casey <Phillip.Casey@navihealth.com>
2018-10-24 15:15:06 -0500

    Map from Entity to Response in Controller

--- API/ConfigurationData/ConfigurationDataController.cs
+++ API/ConfigurationData/ConfigurationDataController.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using AutoMapper;
 using ConfigurationData.Service;
-using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Response = API.ConfigurationData.Models.Response;
 
@@ -11,31 +10,12 @@ namespace API.ConfigurationData
 	public class ConfigurationDataController : ControllerBase
 	{
 		private IConfigurationDataService Service { get; }
+		private IMapper Mapper { get; }
 
-		private Response.ConfigurationDataResponse CreateResponse(ConfigurationDataEntity configurationDataEntity)
-		{
-			var configurationDataResponse = new Response.ConfigurationDataResponse()
-			{
-				Something = configurationDataEntity.Something,
-				ServiceEndpoints = new Dictionary<string, Response.ServiceEndpointDetail>()
-			};
-			return configurationDataResponse;
-		}
-
-		private void CopyServiceEndpoints(ConfigurationDataEntity configurationDataEntity, Response.ConfigurationDataResponse configurationDataResponse)
-		{
-			if (configurationDataEntity.ServiceEndpoints != null)
-			{
-				foreach (var entityEndpoint in configurationDataEntity.ServiceEndpoints)
-				{
-					configurationDataResponse.ServiceEndpoints.Add(entityEndpoint.Key, new Response.ServiceEndpointDetail() { Url = entityEndpoint.Value.Url });
-				}
-			}
-		}
-
-		public ConfigurationDataController(IConfigurationDataService service)
+		public ConfigurationDataController(IConfigurationDataService service, IMapper mapper)
 		{
 			Service = service;
+			Mapper = mapper;
 		}
 
 		[HttpGet]
@@ -43,10 +23,7 @@ namespace API.ConfigurationData
 		{
 			var configurationDataEntity = Service.Lookup();
 
-			//TODO: Automap
-			var configurationDataResponse = CreateResponse(configurationDataEntity);
-			CopyServiceEndpoints(configurationDataEntity, configurationDataResponse);
-
+			var configurationDataResponse = Mapper.Map<Response.ConfigurationDataResponse>(configurationDataEntity);
 			return Ok(configurationDataResponse);
 		}
 	}
---
+++ API/ConfigurationData/Models/Response/ConfigurationDataProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Models;
+
+namespace API.ConfigurationData.Models.Response
+{
+	public class ConfigurationDataProfile : Profile
+	{
+		public ConfigurationDataProfile()
+		{
+			CreateMap<ConfigurationDataEntity, ConfigurationDataResponse>();
+		}
+	}
+}
